5cec4c8ca72a857a018eef561be7c7e78f06851b
adityarajmishra807-cmyk <adityarajmishra807@gmail.com>
2025-12-19 00:52:15 +0530

    Update and rename Day 1 to Day 1 project "band name generator".py

---
+++ Day 1 project "band name generator".py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+city=input("What city did you grew up in?\n")
+pet=input("What is your pet name?\n")
+print("Your band name could be :" + city + " " + pet)
